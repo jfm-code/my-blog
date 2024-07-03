47d8ca20424ed2530d62cb795bb9091ad5286365
jfm-code <my.giangvu@gmail.com>
2024-07-03 10:28:47 +0700

    Use firebase for images

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -4,14 +4,14 @@ export const HomePage = () => {
       <div className="flex flex-col items-center">
         <img className="m-10 w-96" src="./blog-logo-green.svg"></img>
         <p className="text-xl">A personal blog. Created with discipline, wisdom and kindness.</p>
-        <img className="px-40 py-20" src="./home-1.jpg"></img>
+        <img className="px-40 py-20" src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/being-daisy%2Fdaisy-1.jpg?alt=media&token=eac6b9d0-65ed-409d-9742-f74a98ee123d"></img>
       </div>
       <div className="flex flex-row px-40 space-x-16">
         <div className="w-2/3">
           <div className="flex flex-col items-center">
             <p className="text-xl font-medium">JULY 14, 2023</p>
             <p className="text-3xl mt-5 my-10 font-bold">Post title goes here</p>
-            <img className="px-10" src="./home-2.jpg"></img>
+            <img className="w-full px-10" src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/high-school%2Fhighschool-1.jpg?alt=media&token=3c0f89db-9457-4d76-ba3b-e54b089e4ed8"></img>
             <p className="text-lg text-justify my-10">
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
@@ -21,7 +21,7 @@ export const HomePage = () => {
           <div className="flex flex-col items-center">
             <p className="text-xl font-medium">JULY 14, 2023</p>
             <p className="text-3xl mt-5 my-10 font-bold">Post title goes here</p>
-            <img className="px-10" src="./home-2.jpg"></img>
+            <img className="w-full px-10" src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/high-school%2Fhighschool-2.jpg?alt=media&token=d7da3be7-7221-4f21-8dcb-9aef2a219073"></img>
             <p className="text-lg text-justify my-10">
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
@@ -32,7 +32,7 @@ export const HomePage = () => {
         <div className="w-1/3 flex flex-col text-lg text-justify">
           <div className="flex flex-col items-center">
             <p className="text-3xl font-bold">About me</p>
-            <img className="m-10 size-96 rounded-full" src="./home-3.jpg"></img>
+            <img className="m-10 size-96 rounded-full" src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/being-daisy%2Fdaisy-2.jpg?alt=media&token=2c749950-6edf-4b5e-823a-ee5aee1f4db1"></img>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             </p>
@@ -40,10 +40,10 @@ export const HomePage = () => {
           <div className="flex flex-col items-center">
             <p className="text-3xl font-bold mt-14">My latest album</p>
             <div className="grid grid-cols-2 gap-4 m-7">
-              <img className="" src="./home-4.jpg"></img>
-              <img className="" src="./home-5.jpg"></img>
-              <img className="" src="./home-6.jpg"></img>
-              <img className="" src="./home-7.jpg"></img>
+              <img className="" src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/being-daisy%2Fdaisy-3.jpg?alt=media&token=922c27ee-deb4-4050-90d7-1b4be6c670d1"></img>
+              <img className="" src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/being-daisy%2Fdaisy-4.jpg?alt=media&token=b0d35860-8705-4e79-8e00-53b93d5fd157"></img>
+              <img className="" src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/being-daisy%2Fdaisy-5.jpg?alt=media&token=06de1a81-39f5-4a7d-8c36-e83d45668912"></img>
+              <img className="" src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/being-daisy%2Fdaisy-6.jpg?alt=media&token=64739d60-e1cf-4541-a106-61b0812344d5"></img>
             </div>
             <p className="text-xl font-medium">Album name and link goes here</p>
             <p className="my-2">
