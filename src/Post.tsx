95261f93d4671ecc2f3b33de48de54fbbc5f5ea1
jfm-code <my.giangvu@gmail.com>
2024-07-08 22:32:29 +0700

    Finished the post layout

--- src/Post.tsx
+++ src/Post.tsx
@@ -16,32 +16,108 @@ export const Post = () => {
                     <input className="mt-5 mb-16 p-2 w-28 text-lg text-white border border-white bg-[#7c9e83] hover:bg-white hover:text-[#7c9e83] hover:border-[#7c9e83]" type="submit" value="Read more"></input>
                 </div>
             </div>
-            <div className="flex flex-row space-x-20 my-5">
-                <div className="flex flex-col w-1/5">
+            <div className="flex flex-row space-x-20 my-10">
+                <div className="flex flex-col w-1/6">
                     <p className="text-2xl font-semibold mb-2 ml-2">TOPICS</p>
                     <div className="flex items-center ml-8">
-                        <Checkbox id="check1" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none" defaultChecked />
-                        <label htmlFor="check1" className="cursor-pointer">Bla bla</label>
+                        <Checkbox id="education" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none"/>
+                        <label htmlFor="education" className="cursor-pointer text-lg">Education</label>
                     </div>
                     <div className="flex items-center ml-8">
-                        <Checkbox id="check1" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none" defaultChecked />
-                        <label htmlFor="check1" className="cursor-pointer">Bla bla</label>
+                        <Checkbox id="work" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none"/>
+                        <label htmlFor="work" className="cursor-pointer">Work</label>
                     </div>
                     <div className="flex items-center ml-8">
-                        <Checkbox id="check1" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none" defaultChecked />
-                        <label htmlFor="check1" className="cursor-pointer">Bla bla</label>
+                        <Checkbox id="friendship" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none"/>
+                        <label htmlFor="friendship" className="cursor-pointer">Friendship</label>
                     </div>
                     <div className="flex items-center ml-8">
-                        <Checkbox id="check1" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none" defaultChecked />
-                        <label htmlFor="check1" className="cursor-pointer">Bla bla</label>
+                        <Checkbox id="family" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none" />
+                        <label htmlFor="family" className="cursor-pointer">Family</label>
                     </div>
-                   
+                    <div className="flex items-center ml-8">
+                        <Checkbox id="relationship" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none"/>
+                        <label htmlFor="relationship" className="cursor-pointer">Relationship</label>
+                    </div>
+                    <div className="flex items-center ml-8">
+                        <Checkbox id="dream" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none"/>
+                        <label htmlFor="dream" className="cursor-pointer">Dream</label>
+                    </div>
+                    <div className="flex items-center ml-8">
+                        <Checkbox id="psychology" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none"/>
+                        <label htmlFor="psychology" className="cursor-pointer">Psychology</label>
+                    </div>
+                    <div className="flex items-center ml-8">
+                        <Checkbox id="beauty" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none"/>
+                        <label htmlFor="beauty" className="cursor-pointer">Beauty</label>
+                    </div>
+                    <div className="flex items-center ml-8">
+                        <Checkbox id="personality" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none"/>
+                        <label htmlFor="personality" className="cursor-pointer">Personality</label>
+                    </div>
+                    <div className="flex items-center ml-8">
+                        <Checkbox id="justice" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none"/>
+                        <label htmlFor="justice" className="cursor-pointer">Justice</label>
+                    </div>
+                    <div className="flex items-center ml-8">
+                        <Checkbox id="mental" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none"/>
+                        <label htmlFor="mental" className="cursor-pointer">Mental Health</label>
+                    </div>
+                    <div className="flex items-center ml-8">
+                        <Checkbox id="book" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none"/>
+                        <label htmlFor="book" className="cursor-pointer">Book Review</label>
+                    </div>
+                    <div className="flex items-center ml-8">
+                        <Checkbox id="yearend" className="w-6 h-6 rounded-none checked:bg-[#7c9e83] border border-[#7c9e83] border-2 checked:border-none"/>
+                        <label htmlFor="yearend" className="cursor-pointer">Year-End Review</label>
+                    </div>
                 </div>
                 <div className="w-5/6">
-                    <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
+                    <div className="grid grid-cols-3 gap-8">
+                        <div>
+                            <img src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/high-school%2Fhighschool-3.jpg?alt=media&token=3e861c8b-7396-4e59-8044-066e25748230"></img>
+                            <p className="text-xl font-semibold mt-5 mb-2">Post title and link go here</p>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div>
+                            <img src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/high-school%2Fhighschool-1.jpg?alt=media&token=3c0f89db-9457-4d76-ba3b-e54b089e4ed8"></img>
+                            <p className="text-xl font-semibold mt-5 mb-2">Post title and link go here</p>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div>
+                            <img src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/middle-school%2Fmiddle-school-1.jpg?alt=media&token=506736c5-758a-4871-8e44-4f6542fdf6a8"></img>
+                            <p className="text-xl font-semibold mt-5 mb-2">Post title and link go here</p>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div>
+                            <img src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/high-school%2Fhighschool-2.jpg?alt=media&token=d7da3be7-7221-4f21-8dcb-9aef2a219073"></img>
+                            <p className="text-xl font-semibold mt-5 mb-2">Post title and link go here</p>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div>
+                            <img src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/high-school%2Fhighschool-3.jpg?alt=media&token=3e861c8b-7396-4e59-8044-066e25748230"></img>
+                            <p className="text-xl font-semibold mt-5 mb-2">Post title and link go here</p>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                        <div>
+                            <img src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/high-school%2Fhighschool-3.jpg?alt=media&token=3e861c8b-7396-4e59-8044-066e25748230"></img>
+                            <p className="text-xl font-semibold mt-5 mb-2">Post title and link go here</p>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            </p>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
             
