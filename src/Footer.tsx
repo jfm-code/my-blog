60f0c4a9e1a74a30643a9c0d04df30bcb3cd12dc
jfm-code <my.giangvu@gmail.com>
2024-07-22 17:24:57 +0700

    Refactor footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,30 +1,48 @@
 export const Footer = () => {
+
+    const content = [
+        {
+            header:'SECTIONS',
+            content_list: [
+                { name:'About me'},
+                { name:'Posts'},
+                { name:'Album'},
+                { name:'Vlogs'},
+                { name:'Projects'}
+            ]
+        },
+        {
+            header:'TOPICS',
+            content_list: [
+                { name:'Education'},
+                { name:'Work'},
+                { name:'Friendship'},
+                { name:'Family'},
+                { name:'Mental Health'}
+            ]
+        },
+        {
+            header:'ALBUM',
+            content_list: [
+                { name:'Nha Trang 2024'},
+                { name:'Being Daisy'},
+                { name:'LHP Graduation'},
+                { name:'Phan Thiet 2022'},
+                { name:'Da Lat 2021'}
+            ]
+        }
+    ]
+
     return (
         <div className="flex flex-row justify-around bg-primary text-lg custom_sm:text-xl text-primary px-5 custom_md:px-20 custom_xl:px-40">
-            <div className="flex flex-col my-8 custom_lg:my-14 space-y-3 hidden custom_nm:block">
-                <p className="text-white font-bold mb-2">SECTIONS</p>
-                <p className="text-white hover:underline">About me</p>
-                <p className="text-white hover:underline">Posts</p>
-                <p className="text-white hover:underline">Album</p>
-                <p className="text-white hover:underline">Vlogs</p>
-                <p className="text-white hover:underline">Projects</p>
-            </div>
-            <div className="flex flex-col my-8 custom_lg:my-14 space-y-3 hidden custom_lg:block">
-                <p className="text-white font-bold mb-2">TOPICS</p>
-                <p className="text-white hover:underline">Education</p>
-                <p className="text-white hover:underline">Work</p>
-                <p className="text-white hover:underline">Frienship</p>
-                <p className="text-white hover:underline">Family</p>
-                <p className="text-white hover:underline">Mental Health</p>
-            </div>
-            <div className="flex flex-col my-8 custom_lg:my-14 space-y-3 hidden custom_lg:block">
-                <p className="text-white font-bold mb-2">ALBUM</p>
-                <p className="text-white hover:underline">Nha Trang 2024</p>
-                <p className="text-white hover:underline">Being Daisy</p>
-                <p className="text-white hover:underline">LHP Graduation</p>
-                <p className="text-white hover:underline">Phan Thiet 2022</p>
-                <p className="text-white hover:underline">Da Lat 2021</p>
-            </div>
+            {content.map(section => (
+                <div className="flex flex-col my-8 custom_lg:my-14 space-y-3 hidden custom_nm:block">
+                    <p className="text-white font-bold mb-2">{section.header}</p>
+                    {section.content_list.map(line => (
+                        <p className="text-white hover:underline">{line.name}</p>
+                    ))}
+                </div>
+            ))}
             <form className="pl-3 custom_sm:pl-0 my-8 custom_lg:my-14 space-y-8">
                 <p className="text-white font-bold">SUBSCRIBE FOR EMAILS FROM JFM BLOG</p>
                 <div className="space-y-1">
