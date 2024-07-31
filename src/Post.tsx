05400ba78bb50492bc9348ca83586b5885f01c00
jfm-code <my.giangvu@gmail.com>
2024-07-31 18:07:03 +0700

    Make the filter in post component work

--- src/Post.tsx
+++ src/Post.tsx
@@ -10,39 +10,71 @@ import { useLanguage } from './LanguageContext';
 export const Post = () => {  
     const [topics, setTopics] = useState<TopicObject[]>([]);
     const [posts, setPosts] = useState<PostObject[]>([]);
+    const [currentTopics, setCurrentTopics] = useState<string[]>([]);
     const { currentLanguage } = useLanguage();
 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const querySnapshot1 = await getDocs(collection(db, "topics"));
-            const querySnapshot2 = await getDocs(collection(db, "posts"));
+    const selectTopic = (topic: string) => {
+        console.log("topic passing in", topic);
 
-            const albumsData: TopicObject[] = [];
-            const postsData: PostObject[] = [];
+        setCurrentTopics(prevTopics => {
+            // Create a copy of the previous state
+            const updatedTopics = [...prevTopics];
+            // Check if the topic is already in the list
+            const index = updatedTopics.indexOf(topic.toLocaleLowerCase());
+            if (index === -1) {
+                updatedTopics.push(topic.toLocaleLowerCase());
+            } else {
+                // Remove topic if already in the list
+                updatedTopics.splice(index, 1);
+            }
+            return updatedTopics;
+        });
+    };
 
-            querySnapshot1.forEach((doc) => {
-              albumsData.push(doc.data() as TopicObject);
-            });
-            setTopics(albumsData);
+    const fetchData = async () => {
+        try {
+        const querySnapshot1 = await getDocs(collection(db, "topics"));
+        const querySnapshot2 = await getDocs(collection(db, "posts"));
 
-            querySnapshot2.forEach((doc) => {
+        const albumsData: TopicObject[] = [];
+        let postsData: PostObject[] = [];
+
+        querySnapshot1.forEach((doc) => {
+            albumsData.push(doc.data() as TopicObject);
+        });
+        setTopics(albumsData);
+
+        let tempPost: PostObject;
+        querySnapshot2.forEach((doc) => {
+            console.log("array size: ", currentTopics.length);
+            if (currentTopics.length > 0) {
+                tempPost = doc.data() as PostObject;
+                for (const tag of tempPost.tags) {
+                    if (currentTopics.includes(tag)) {
+                        postsData.push(tempPost);
+                        break;
+                    }
+                }
+            }
+            else {
                 postsData.push(doc.data() as PostObject);
-              });
-            setPosts(postsData);
+            }
+        });
+        setPosts(postsData);
 
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
-    
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+
+    useEffect(() => {
         fetchData();
-    }, []);
+    }, [currentTopics]);
 
     return (
         <div className="flex flex-col items-center px-5 custom_sm:px-10 custom_md:px-20 custom_xl:px-40 text-justify text-lg text-primary">
             <p className="w-full flex justify-center text-5xl custom_sm:text-6xl custom_nm:text-7xl custom_md:text-8xl font-fontAutography pb-10 custom_sm:py-10">a place to write</p>
-            <div className="w-full w-full flex flex-col custom_md:flex-row custom_md:space-x-8 custom_lg:space-x-16">
+            <div className="w-full flex flex-col custom_md:flex-row custom_md:space-x-8 custom_lg:space-x-16">
                 <img className="flex self-center w-full custom_nm:w-4/5 custom_md:self-start custom_md:w-2/5 custom_md:h-full" src="https://firebasestorage.googleapis.com/v0/b/jfm-blog.appspot.com/o/dalat-2021%2Fdalat-2021-1.JPG?alt=media&token=06767790-e40c-4c87-9d38-06618e011104"></img>
                 <div className="flex flex-col mt-5 w-full custom_nm:w-4/5 self-center custom_md:mt-0 ">
                     <p className="text-xl custom_sm:text-2xl font-medium">Post title and link goes here</p>
@@ -52,17 +84,17 @@ export const Post = () => {
                     <input className="mt-5 mb-10 p-2 w-28 text-lg border border-white text-white bg-primary hover:bg-white hover:text-primary hover:border-primary" type="submit" value="Read more"></input>
                 </div>
             </div>
-            <div className="w-full flex flex-col flex-auto mb-10 custom_lg:flex-row custom_lg:space-x-20 custom_lg:my-10">
-                <div className="flex flex-col mb-5 custom_lg:mb-0">
-                    <p className="flex self-center custom_lg:self-start ml-10 text-2xl font-semibold mb-2 ml-2">TOPICS</p>
-                    <div className="w-full flex justify-between flex-wrap custom_lg:flex-col w-1/6">
+            <div className="w-full flex flex-col flex-auto mb-10 pt-5 custom_lg:flex-row custom_lg:space-x-10 custom_lg:my-10">
+                <div className="flex flex-col mb-5 mx-14 custom_lg:mx-0 custom_lg:mb-0">
+                    <p className="flex self-center custom_lg:self-start text-2xl font-semibold mb-2 ml-2">TOPICS</p>
+                    <div className="w-full custom_lg:w-1/6 flex justify-start flex-wrap custom_lg:flex-col">
                         {topics.map(topic => (
-                            <div className="flex items-center ml-8 min-w-52">
-                                <Checkbox className="w-6 h-6 rounded-none checked:bg-primary border border-primary border-2 checked:border-none" onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined}/>
+                            <div className="flex items-center ml-4 min-w-52">
+                                <Checkbox onClick={() => selectTopic(topic.name.EN)} className="w-6 h-6 rounded-none checked:bg-primary border-primary border-2 checked:border-none" onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined}/>
                                 <label className="cursor-pointer text-lg">{currentLanguage === "EN" ? topic.name.EN : topic.name.VN}</label>
                             </div>
                         ))}
-                </div>
+                    </div>
                 </div>
                 <div className="flex self-center w-full custom_nm:w-11/12 custom_lg:w-5/6">
                     <div className="grid custom_nm:grid-cols-2 custom_lg:grid-cols-3 gap-5">
