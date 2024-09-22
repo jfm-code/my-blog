79cbf1e24f17fe9f4a0eae984686384a04dbc439
jfm-code <my.giangvu@gmail.com>
2024-09-22 14:28:42 -0400

    Add btn in about me

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -9,6 +9,16 @@ export const AboutMe = () => {
 
     const [aboutMeInfo, setAboutMeInfo] = useState<AboutMeObject | null>(null);
     const { currentLanguage } = useLanguage();
+    const [isExpanded1, setIsExpanded1] = useState(false);
+    const [isExpanded2, setIsExpanded2] = useState(false);
+    const viewportWidth = window.innerWidth;
+
+    const handleReadMoreBtn1 = ()  => {
+        setIsExpanded1(!isExpanded1);
+    }
+    const handleReadMoreBtn2 = ()  => {
+        setIsExpanded2(!isExpanded2);
+    }
 
     useEffect(() => {
         const fetchData = async () => {
@@ -23,27 +33,60 @@ export const AboutMe = () => {
                 console.error("Error fetching data in about me component: ", error);
             }
         };
-
+        console.log(viewportWidth)
         fetchData();
-    }, []);
+    }, [viewportWidth, isExpanded1]);
 
     return (
         <div className="flex flex-col items-center text-justify common-style">
             <p className="heading-1">a place to know</p>
             <img className="size-64 custom_sm:size-80 rounded-full" src={aboutMeInfo?.profilepic_link}></img>
             <div className="py-10">
-                {aboutMeInfo?.overview_myself.map(paragraph => (
+                {viewportWidth > 500 && 
+                    aboutMeInfo?.overview_myself.map(paragraph => (
                     <p className="py-1">{currentLanguage === "EN" ? paragraph.EN : paragraph.VN}</p>
                 ))}
+                {viewportWidth <= 500 && 
+                    <div className="flex flex-col items-center">
+                        {isExpanded1 ? 
+                            (aboutMeInfo?.overview_myself.map(paragraph => (
+                                <p className="py-1">{currentLanguage === "EN" ? paragraph.EN : paragraph.VN}</p>
+                            )))
+                            : 
+                            (<p className="py-1">{currentLanguage === "EN" ? aboutMeInfo?.overview_myself[0].EN+'...' : aboutMeInfo?.overview_myself[0].VN+'...'}</p>)
+                        }
+                        <input
+                            className="button" type="submit"
+                            value={currentLanguage === "EN" ? (isExpanded1 ? "Show less" : "Read more") : (isExpanded1 ? "Ẩn bớt" : "Đọc thêm")}
+                            onClick={handleReadMoreBtn1}
+                        ></input>
+                    </div>
+                }
             </div>
             <p className="self-start heading-2">{currentLanguage === "EN" ? "How this blog was born" : "Sự ra đời của trang blog này"}</p>
             <div className="w-full flex flex-col-reverse custom_lg:flex-row space-x-0 custom_lg:space-x-10 custom_xl:space-x-14 items-center mt-3 mb-8">
                 <div className="custom_lg:w-3/4 flex flex-col mt-5 custom_lg:mt-0">
-                    {aboutMeInfo?.overview_blog.map(paragraph => (
+                    {viewportWidth > 500 && aboutMeInfo?.overview_blog.map(paragraph => (
                         <p className="py-1">{currentLanguage === "EN" ? paragraph.EN : paragraph.VN}</p>
                     ))}
+                    {viewportWidth <= 500 &&
+                        <div className="flex flex-col items-center">
+                            {isExpanded2 ? 
+                                (aboutMeInfo?.overview_blog.map(paragraph => (
+                                    <p className="py-1">{currentLanguage === "EN" ? paragraph.EN : paragraph.VN}</p>
+                                )))
+                                :
+                                (<p className="py-1">{currentLanguage === "EN" ? aboutMeInfo?.overview_blog[0].EN.slice(0,390)+'...' : aboutMeInfo?.overview_blog[0].VN.slice(0,390)+'...'}</p>)
+                            }
+                            <input
+                                className="button" type="submit"
+                                value={currentLanguage === "EN" ? (isExpanded2 ? "Show less" : "Read more") : (isExpanded2 ? "Ẩn bớt" : "Đọc thêm")}
+                                onClick={handleReadMoreBtn2}
+                            ></input>
+                        </div>
+                    }
                 </div>
-                <div className="custom_lg:w-1/4 mt-10 custom_lg:mt-0">
+                <div className="custom_lg:w-1/4 mt-2 custom_lg:mt-0">
                     <img className="size-64 custom_sm:size-80 custom_lg:size-auto rounded-full" src={aboutMeInfo?.blogpic_link}></img>
                 </div>
             </div>
